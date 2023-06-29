2d1b0135f1ce7d5e6c9537a4583d2657ec0205b5
cktalala <boompetbank@gmail.com>
2023-06-29 17:15:44 +0700

    update-video

--- resources/js/Pages/Creator.jsx
+++ resources/js/Pages/Creator.jsx
@@ -11,7 +11,7 @@ export default function Creator({}) {
                                 className="w-[9.8125rem] h-[9.8125rem] mt-20 ml-20"
                                 src="/image/circle.png"
                             />
-                            <h1 className="font-bold text-[6rem] w-[22.8125rem] leading-[6rem] ml-20 mt-10">
+                            <h1 className="font-bold text-[6rem] w-[22.8125rem] leading-[6rem] ml-20 mt-10 bg-gradient-to-r from-indigo-950 to-sky-400 bg-clip-text text-transparent">
                                 ฐิติวรรณ เฮงจำรัส
                             </h1>
                             <h1 className="text-[1.5rem] ml-20 mt-5">
@@ -28,6 +28,170 @@ export default function Creator({}) {
                             TRUCK&shy;ROUNDING
                         </h1>
                     </div>
+                </div>
+            </div>
+            <div className="container mx-auto h-full">
+                <p className="underline underline-offset-8 uppercase text-[2rem] text-center mt-24">
+                    WEB Application
+                </p>
+                <div className="flex h-full overflow-hidden">
+                    <div className="w-1/2 ">
+                        <img
+                            src="/image/Groupcom.png"
+                            className="w-[73.0625rem] mt-8"
+                        />
+                    </div>
+                    <div className="w-1/2">
+                        <p className="mt-28 font-bold uppercase text-[2rem] text-indigo-950">
+                            Animal Game
+                        </p>
+                        <p className="mt-10 text-blue-400 text-[1.875rem]">
+                            Design and Development of Physical and Sexual Crimes
+                            Alert Application
+                        </p>
+                        <p className="font-extralight text-[1.875rem]">
+                            แอปพลิเคชั่นที่คุณสามารถค้นหาสินค้า
+                            หรือโปรโมชั่นที่ใกล้เคียง และสามารถรับโปรโมชั่น
+                            ที่คุณต้องการผ่านระบบ QR CODE
+                            เพื่อนำไปรับโปรโมชั่นที่ ร้านค้าที่ต้องการได้
+                            เพื่ออำนวยสะดวกสบายให้คุณ
+                        </p>
+                        <p className="mt-7 font-extralight text-[1rem]">
+                            Design & Development — Thanatron
+                        </p>
+                        <div className="flex mt-14">
+                            <div class="w-[6rem] h-[6rem]">
+                                <img src="/image/htmlpic.png" />
+                            </div>
+                            <div class="w-[6rem] h-[6rem]">
+                                <img src="/image/htmlpic.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container mx-auto h-full">
+                <p className="text-[2rem] mt-32 ml-36">FUCTION</p>
+                <p className="text-[3rem] w-[41.1875rem] ml-36 capitalize font-bold text-indigo-950">
+                    functions available on the website.
+                </p>
+                <div className="flex flex-row m-36">
+                    <div className="basis-1/3 mr-6">
+                        <p className="text-[8rem] font-bold bg-gradient-to-r from-indigo-950 to-sky-400 bg-clip-text text-transparent">
+                            01
+                        </p>
+                        <p className="text-[1.875rem] font-bold uppercase text-teal-600">
+                            Shop Management
+                        </p>
+                        <p className="text-[1.5rem] font-extralight">
+                            จัดการร้านค้า สามารถเพิ่มลบหรือแก้ไข
+                            สินค้าและโปรโมชันในร้านได้
+                        </p>
+                    </div>
+                    <div className="basis-1/3  mr-6">
+                        <p className="text-[8rem] font-bold bg-gradient-to-r from-indigo-950 to-sky-400 bg-clip-text text-transparent">
+                            02
+                        </p>
+                        <p className="text-[1.875rem] font-bold uppercase text-teal-600">
+                            Shop Management
+                        </p>
+                        <p className="text-[1.5rem] font-extralight">
+                            จัดการร้านค้า สามารถเพิ่มลบหรือแก้ไข
+                            สินค้าและโปรโมชันในร้านได้
+                        </p>
+                    </div>
+                    <div className="basis-1/3">
+                        <p className="text-[8rem] font-bold bg-gradient-to-r from-indigo-950 to-sky-400 bg-clip-text text-transparent">
+                            03
+                        </p>
+                        <p className="text-[1.875rem] font-bold uppercase text-teal-600">
+                            Shop Management
+                        </p>
+                        <p className="text-[1.5rem] font-extralight">
+                            จัดการร้านค้า สามารถเพิ่มลบหรือแก้ไข
+                            สินค้าและโปรโมชันในร้านได้
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-row">
+                    <div className="basis-1/12">
+                        <div className="w-[11.25rem] h-[0.0625rem] bg-black ml-20 mt-7"></div>
+                    </div>
+                    <div className="basis-10/12">
+                        <p className="uppercase text-[2.25rem] text-center">
+                            advisor — KACHORNPON Hiranchotepaisan
+                        </p>
+                    </div>
+                    <div className="basis-1/12">
+                        <div className="w-[11.25rem] h-[0.0625rem] bg-black mr-20 mt-7"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="container mx-auto h-full">
+                <p className="mt-32 text-center capitalize font-bold text-[6rem] mb-9">
+                    Video
+                </p>
+                <div className="w-[84.125rem] h-[47.3125rem] bg-black mx-auto rounded-md">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        src="https://www.youtube.com/embed/xOAGH2gbhOA"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen
+                    ></iframe>
+                </div>
+                <div className="mx-auto">
+                    <div className="flex h-full mt-9">
+                        <div className="w-1/2">
+                            <p className="ml-20 text-[2.5rem] font-bold capitalize">
+                                Showreel
+                            </p>
+                        </div>
+                        <div className="w-1/2">
+                            <p className=" text-right mr-20 text-[2.5rem] font-bold capitalize">
+                                Thanatron (2023)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto">
+                    <div className="flex h-full mt-[5.25rem]">
+                        <div className="w-1/2">
+                            <div className="w-[38.125rem] h-[21.4375rem] bg-black mx-auto rounded-md">
+                                <iframe
+                                    width="100%"
+                                    height="100%"
+                                    src="https://www.youtube.com/embed/2jSVlBNpE_U"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allowfullscreen
+                                ></iframe>
+                            </div>
+                            <p className="ml-20 text-[2.5rem] font-bold capitalize mt-10">
+                                Demo
+                            </p>
+                        </div>
+                        <div className="w-1/2">
+                            <div className="w-[38.125rem] h-[21.4375rem] bg-black mx-auto rounded-md">
+                                <iframe
+                                    width="100%"
+                                    height="100%"
+                                    src="https://www.youtube.com/embed/Z0QModZv_C8"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allowfullscreen
+                                ></iframe>
+                            </div>
+                            <p className="ml-20 mr-20 text-[2.5rem] font-bold capitalize mt-10">
+                                Other
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[84.3125rem] h-[0.0625rem] bg-black mx-auto mt-[5.12rem]"></div>
                 </div>
             </div>
         </>
